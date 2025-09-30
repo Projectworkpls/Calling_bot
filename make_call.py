8877f488c337e1e13c2dfc88c70356e60b79a03e
Parth Acharya <125336330+Projectworkpls@users.noreply.github.com>
2025-09-30 09:52:56 +0530

    Update make_call.py

--- make_call.py
+++ make_call.py
@@ -3,7 +3,7 @@ import requests
 url = "https://calling-bot.onrender.com/start-call"
 
 payload = {
-    "to": "+91xxxxxxxxxx",  # Replace with the phone number you want to call, with country code
+    "to": "+919663192931",  # Replace with the phone number you want to call, with country code
     "webhook_url": "https://calling-bot.onrender.com/call-events"  # Your backend webhook to handle call events
 }
 
@@ -15,3 +15,4 @@ response = requests.post(url, json=payload, headers=headers)
 
 print("Status Code:", response.status_code)
 print("Response:", response.json())
+
